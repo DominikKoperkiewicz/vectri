6b42f3ab1420019aa8f7ac3c35ccb0771a61431e
Dominik Koperkiewicz <254023@student.pwr.edu.pl>
2022-10-30 18:54:03 +0100

    PixiJS canvas added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 const Application = PIXI.Application;
 
 const app = new Application({
+    resizeTo: document.getElementById("viewport"),
     width: 500,
     height: 500,
     transparent: false,
-    antialias: true
+    antialias: true,
+    backgroundColor: 0xffffff
 });
 
-//document.body.appendChild(app.view);
+document.getElementById("viewport").appendChild(app.view);
+
+let surface = document.getElementById("viewport").children[0];
+
+surface.style.position = "absolute";
